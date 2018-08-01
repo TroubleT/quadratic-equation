7830e7aa293754e50de706d9c1f38e95fe06d5ac
Hryhoriy Starykov <starykovhryhoriy@gmail.com>
2018-08-01 13:44:21 +0000

    fixed

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
-const a = parseInt(prompt('Enter number:'));
-const b = parseInt(prompt('Enter number:'));
-const c = parseInt(prompt('Enter number:'));
-let x1
-let x2
+const a = parseFloat(prompt('Enter number:'));
+const b = parseFloat(prompt('Enter number:'));
+const c = parseFloat(prompt('Enter number:'));
 
 function quadraticE(a, b, c) {
-    let d = b * b - 4 * a * c;
+    const d = b * b - 4 * a * c;
     if (d > 0) {
-        x1 = (-b - Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
-        let result = String(x1) + 'and' + String(x2);
-        alert('You get two roots:' + result);
+        const x1 = (-b - Math.sqrt(d)) / (2 * a);
+        const x2 = (b - Math.sqrt(d)) / (2 * a);
+        const result = String(x1) + 'and' + String(x2);
+        return ('You get two roots:' + result);
     }
     else if (d === 0) {
-        x1 = x2 = -b / (2 * a);
-        let result = String(x1);
-        alert('You get one root: ' + result);
+       const x1 = -b / (2 * a);
+        const result = String(x1);
+        return ('You get one root: ' + result);
     }
     else {
-        alert('There are no real roots! Give me 2$ to get complex roots');
+        return ('There are no real roots! Give me 2$ to get complex roots');
     }
 }
-quadraticE(a, b, c);
+const result = quadraticE(a, b, c);
+alert(result)
